b0e5c6f4b5c201f8eeea65802590fbc13eae1afe
Jzakka <mouse4786@gmail.com>
2023-03-10 16:46:31 +0900

    test: 테스트케이스 추가

--- src/test/java/org/example/EvaluatorTest.java
+++ src/test/java/org/example/EvaluatorTest.java
@@ -1,13 +1,23 @@
 package org.example;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class EvaluatorTest {
-
+    class Calc{
+        public static long run(String exp){
+            return new Evaluator(exp).eval();
+        }
+    }
+    @Test
+    @DisplayName("10 + 20 + 30 == 60")
+    void t7() {
+        assertThat(Calc.run("10 + 20 + 30")).isEqualTo(60);
+    }
     @Test
     void test1() {
         assertThat(new Evaluator("0+1").eval()).isEqualTo(1);
@@ -42,4 +52,11 @@ class EvaluatorTest {
     void test7(){
         assertThat(new Evaluator("1 + 2+(3*(8+2)) -2").eval()).isEqualTo(31);
     }
+
+    @Test
+    void test8(){
+        assertThat(new Evaluator("1004 - ((7-9)*9 + 225-9*8)*1").eval()).isEqualTo(869);
+    }
+
+    // 1004   - (-18 + 225 - 72) = 1004 -(225 - 90) = 1004 - 135 = 869
 }
